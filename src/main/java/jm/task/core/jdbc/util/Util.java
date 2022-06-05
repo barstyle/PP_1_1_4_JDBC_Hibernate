1f1b08ab84cf1d9d944e09b0a9d3d0ed8c3924b2
Georgiy Goloborodov <barstyle@mail.ru>
2022-06-05 10:27:19 +0300

    fixed class Util

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,17 +1,30 @@
 package jm.task.core.jdbc.util;
 
 import java.sql.Connection;
-import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Util {
     // реализуйте настройку соединения с БД
+
+    private static Connection connection = null;
+
     private static final String PASSWORD = "qwerty007";
     private static final String USER_NAME = "BD_Admin";
     private static final String URL = "jdbc:mysql://localhost:3306/kata_first";
 
-    public static Connection getMySQLConnection() throws SQLException {
-        return DriverManager.getConnection(URL, USER_NAME, PASSWORD);
+    public static Connection getMySQLConnection() {
+        try {
+            if (connection == null || connection.isClosed()) {
+                try {
+                    connection = DriverManager.getConnection(URL, USER_NAME, PASSWORD);
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
     }
 }
